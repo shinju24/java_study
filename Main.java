fb7239b7c005fe38821136a4737bd77b0aad1ac4
shinju <shinju2024@gmail.com>
2021-10-01 23:59:59 +0900

    標準出力とかいろいろ試してるけどわからなくなっちゃった

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 class Main {
   public static void main (String args[]) {
-    Triangle test = new Triangle();
-    System.out.println(test.calc(10,20));
+    Triangle triangle = new Triangle();
+    StandardInput inputBase = new StandardInput();
+    StandardInput inputHeight = new StandardInput();
+    System.out.println(triangle.calc(inputBase.inputNum(),20));
+    // StandardInput input = new StandardInput();
+    // input.inputStr();
   }
 }
---
+++ StandardInput.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class StandardInput {
+  Scanner scan = new Scanner(System.in);
+  // String inputStr () {
+  //   String str = scan.nextLine();
+  //   System.out.println(str);
+  //   return str;
+  // }
+
+  int inputNum() {
+    Scanner scan = new Scanner(System.in);
+    scan.close();
+    return 0;
+  }
+}
